370defb3e1149922297872509007691f3098ec05
Lukas Wieg <lukaswieg@googlemail.com>
2024-04-16 21:29:37 +0200

    Add a custom runner exception class.

--- NukeTestRunner/run_tests.py
+++ NukeTestRunner/run_tests.py
@@ -4,6 +4,10 @@ import subprocess
 from pathlib import Path
 
 
+class RunnerException(Exception):
+    """Exception class for testrunner related exceptions."""
+
+
 class Runner:
     """Testrunner for nuke.
 
@@ -20,18 +24,31 @@ class Runner:
             test_files: path to the test files.
         """
         nuke_path = Path(nuke_executable)
-        assert nuke_path.exists(), "Provided nuke path does not exist."
-        assert (
-            nuke_path.stem.lower() == "nuke"
-        ), "Provided nuke path is not pointing to a nuke executable."
-        if (self.is_windows() and nuke_path.suffix == ".sh") or (
-            not self.is_windows() and nuke_path.suffix in [".exe", ".bat"]
-        ):
-            msg = "Provided path is incompatible with your os."
-            raise OSError(msg)
+        self._check_nuke_executable(nuke_path)
 
         self._executable = nuke_path
         self._test_files = test_files
+
+    def _check_nuke_executable(self, nuke_path: Path):
+        """Check that the nuke path is executable on the current system.
+
+        Args:
+            nuke_path: path to the nuke executable.
+
+        Raises:
+            RunnerException if path is not executable.
+        """
+        if not nuke_path.exists():
+            msg = "Provided nuke path does not exist."
+            raise RunnerException(msg)
+        if nuke_path.stem.lower() != "nuke":
+            msg = "Provided nuke path is not pointing to a nuke executable."
+            raise RunnerException(msg)
+        if (self.is_windows() and nuke_path.suffix == ".sh") or (
+                not self.is_windows() and nuke_path.suffix in [".exe", ".bat"]
+        ):
+            msg = "Provided path is incompatible with your os."
+            raise RunnerException(msg)
 
     def execute_tests(self) -> int:
         """Run the testrunner with provided arguments."""
