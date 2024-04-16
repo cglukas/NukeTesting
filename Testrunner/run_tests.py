9f32d2dde923954b64626d7ecf01dd406befe079
Lukas Wieg <lukaswieg@googlemail.com>
2024-04-16 21:21:54 +0200

    Implement os check for testrunner.

--- Testrunner/run_tests.py
+++ Testrunner/run_tests.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import os
 import subprocess
 from pathlib import Path
 
@@ -24,20 +24,24 @@ class NukeRunner:
         assert (
             nuke_path.stem.lower() == "nuke"
         ), "Provided nuke path is not pointing to a nuke executable."
+        if (self.is_windows() and nuke_path.suffix == ".sh") or (
+            not self.is_windows() and nuke_path.suffix in [".exe", ".bat"]
+        ):
+            msg = "Provided path is incompatible with your os."
+            raise OSError(msg)
+
         self._executable = nuke_path
         self._test_files = test_files
 
     def execute_tests(self) -> int:
         """Run the testrunner with provided arguments."""
         try:
-            subprocess.check_call(
-                [
-                    "nuke",
-                    "-t",
-                    self.TEST_SCRIPT,
-                    self._test_files
-                ]
-            )
+            subprocess.check_call(["nuke", "-t", self.TEST_SCRIPT, self._test_files])
         except subprocess.CalledProcessError as err:
             return err.returncode
         return 0
+
+    @staticmethod
+    def is_windows() -> bool:
+        """Check if the operating system is windows."""
+        return os.name == "nt"
