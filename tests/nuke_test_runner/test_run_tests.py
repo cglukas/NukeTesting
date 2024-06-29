be61b39eece51908a34a15fe202a48968996746d
Lukas Wieg <lukaswieg@googlemail.com>
2024-06-29 15:50:43 +0200

    Fix order of import checks for skipping pytest.

--- tests/nuke_test_runner/test_run_tests.py
+++ tests/nuke_test_runner/test_run_tests.py
@@ -10,13 +10,14 @@ import pytest
 
 from datamodel import constants
 from nuke_test_runner.runner import Runner
-from run_tests import run_tests
 
 pytest.importorskip(
     "run_tests",
     reason="This module is not importable by the nuke runtime. "
     "Skip tests of this module when executing them with nuke.",
 )
+
+from run_tests import run_tests
 
 
 @pytest.fixture()
