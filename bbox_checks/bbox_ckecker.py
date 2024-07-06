809013a55f67d5a084d6b7fa620ef772fc0e732a
Lukas Wieg <lukaswieg@googlemail.com>
2024-07-06 12:10:44 +0200

    Implement function to compare bbox with lists, strings or box object.

--- bbox_checks/bbox_ckecker.py
+++ bbox_checks/bbox_ckecker.py
@@ -1,5 +1,7 @@
 """Module for comparing bbox data."""
 
+from __future__ import annotations
+
 import nuke
 
 
@@ -18,12 +20,77 @@ def assert_same_bbox(node_a: nuke.Node, node_b: nuke.Node) -> None:
     values_a = [bbox_a.x(), bbox_a.y(), bbox_a.x() + bbox_a.w(), bbox_a.y() + bbox_a.h()]
     values_b = [bbox_b.x(), bbox_b.y(), bbox_b.x() + bbox_b.w(), bbox_b.y() + bbox_b.h()]
 
+    all_messages = __get_missmatching_information(values_a, values_b)
+
+    if all_messages:
+        msg = f"Node '{node_a.name()}' and node '{node_b.name()}' have mismatches in their bbox: {' and '.join(all_messages)}"
+        raise AssertionError(msg)
+
+
+def __get_missmatching_information(values_a: list[int], values_b: list[int]) -> list[str]:
+    """Get a list of messages about missmatching values.
+
+    Args:
+        values_a: four values representing left, bottom, right and top of the boundingbox.
+        values_b: four values representing left, bottom, right and top of the boundingbox.
+
+    Returns:
+        The corresponding messages for found missmatches.
+    """
     attributes = ["on the left side", "at the bottom", "on the right side", "at the top"]
     all_messages = []
     for a, b, msg in zip(values_a, values_b, attributes):
         if a != b:
             all_messages.append(msg)
+    return all_messages
+
+
+def assert_bbox_shape(node: nuke.Node, shape: list[int] | nuke.Box | str) -> None:
+    """Assert that the node bbox matches the provided shape.
+
+    Args:
+        node: The node to check.
+        shape: A bounding box shape in the order [left, bottom, right, top]
+            Strings are supported but need to follow the same ordering. Each value separated by a whitespace.
+    """
+    bbox_a = node.bbox()
+    values_a = [bbox_a.x(), bbox_a.y(), bbox_a.x() + bbox_a.w(), bbox_a.y() + bbox_a.h()]
+    values_b = []
+
+    required_value_count = 4
+    if isinstance(shape, list):
+        if len(shape) != required_value_count:
+            msg = f"Expected four values for bounding box check, you provided {len(shape)}."
+            raise ValueError(msg)
+
+        if not all(isinstance(entry, int) for entry in shape):
+            msg = f"Only list of ints supported, you provided: '{shape}'"
+            raise TypeError(msg)
+        values_b = shape
+
+    elif isinstance(shape, nuke.Box):
+        values_b = [shape.x(), shape.y(), shape.r(), shape.t()]
+
+    elif isinstance(shape, str):
+        split = shape.split(" ")
+        if len(split) != required_value_count:
+            msg = f"Expected four values for bounding box check, you provided {len(split)}."
+            raise ValueError(msg)
+        try:
+            values_b = [int(elem) for elem in split]
+        except ValueError as e:
+            msg = f"Expected string containing only numeric values. You provided '{shape}'"
+            raise TypeError(msg) from e
+    else:
+        msg = f"The type '{type(shape)}' is not supported."
+        raise TypeError(msg)
+
+    all_messages = __get_missmatching_information(values_a, values_b)
 
     if all_messages:
-        msg = f"Node '{node_a.name()}' and node '{node_b.name()}' have mismatches in their bbox: {'and'.join(all_messages)}"
+        msg = (
+            f"Node '{node.name()}' does not match the required bounding box: '{shape}':\n"
+            f"BBox '{values_a}' != '{values_b}'.\n"
+            f"The missmatches are: {' and '.join(all_messages)}"
+        )
         raise AssertionError(msg)
--- tests/test_bbox_ckecker.py
+++ tests/test_bbox_ckecker.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+
 import pytest
 
 nuke = pytest.importorskip("nuke")
 
-from bbox_checks.bbox_ckecker import assert_same_bbox
+from unittest.mock import MagicMock
+
+from bbox_checks.bbox_ckecker import assert_bbox_shape, assert_same_bbox
 
 
 def test_assert_same_bbox() -> None:
@@ -42,3 +46,44 @@ def test_assert_same_bbox_multiple_missmatch(reference: nuke.Node, bbox: str, me
 
     for msg in message:
         assert error.match(msg)
+
+
+@pytest.mark.parametrize("shape", [[0, 0, 1920, 1080], nuke.Box(x=0, y=0, r=1920, t=1080), "0 0 1920 1080"])
+def test_assert_bbox_shape(shape: list[int] | nuke.Box | str) -> None:
+    """Test that the bbox can be compared to a manual definition."""
+    reference = nuke.nodes.Crop(
+        box="0 0 1920 1080",
+        crop=False,  # Prevent nuke from adding an extra black pixel around the image, thereby increasing the bbox.
+    )
+    assert_bbox_shape(reference, shape)
+
+
+class TestAssertBBoxShapeWrongInput:
+    """Test concerning possible wrong inputs for the function."""
+
+    @pytest.mark.parametrize(
+        ("wrong_string", "exception"),
+        [("", ValueError), ("1 1 1", ValueError), ("a 1 1 1", TypeError), ("1 1 1 1 1", ValueError)],
+    )
+    def test_strings(self, wrong_string: str, exception: type) -> None:
+        """Test that wrong values in strings raise exceptions."""
+        node = MagicMock(spec=nuke.Node)
+        with pytest.raises(exception):
+            assert_bbox_shape(node, wrong_string)
+
+    @pytest.mark.parametrize(
+        ("wrong_list", "exception"),
+        [([], ValueError), ([1, 1, 1], ValueError), ([1, "1", 1, 1], TypeError), ([1, 1, 1, 1, 1], ValueError)],
+    )
+    def test_lists(self, wrong_list: list, exception: type) -> None:
+        """Test that wrong values or length of lists raise exceptions."""
+        node = MagicMock(spec=nuke.Node)
+        with pytest.raises(exception):
+            assert_bbox_shape(node, wrong_list)
+
+    @pytest.mark.parametrize("wrong_instance", [object(), MagicMock(), (), set(), {}])
+    def test_wrong_type(self, wrong_instance: object) -> None:
+        """Test that unsupported types raise type errors."""
+        node = MagicMock(spec=nuke.Node)
+        with pytest.raises(TypeError):
+            assert_bbox_shape(node, wrong_instance)
