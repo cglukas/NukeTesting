6ac6601c58bcb610b6796c36e80640ddc808c684
Lukas Wieg <lukaswieg@googlemail.com>
2024-07-06 14:46:12 +0200

    Implement parameter to check specific inputs.

--- tests/test_tree_checker.py
+++ tests/test_tree_checker.py
@@ -20,3 +20,23 @@ def test_is_connected() -> None:
 
     assert not is_connected(a, c), "Unconnected nodes should return False."
     assert not is_connected(None, c), "The unconnected case should return False."
+
+
+def test_is_connected_at() -> None:
+    """Test that connection can be checked for a specific input."""
+    a = nuke.nodes.NoOp()
+    b = nuke.nodes.Switch(inputs=[None, None, a])  # Has multiple inputs.
+
+    assert is_connected(a, b)
+    assert is_connected(a, b, at=2)
+    assert not is_connected(a, b, at=0)
+    assert not is_connected(a, b, at=1)
+
+
+@pytest.mark.parametrize("index", [-1, 2, 3])
+def test_is_connected_at_out_of_range(index: int) -> None:
+    """Test that it raises an exception if inputs outside the available range are checked."""
+    a = nuke.nodes.NoOp()
+    b = nuke.nodes.NoOp()
+    with pytest.raises(IndexError):
+        assert not is_connected(a, b, at=index)
--- tree_checks/tree_checker.py
+++ tree_checks/tree_checker.py
@@ -1,14 +1,25 @@
-import nuke
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import nuke
 
 
-def is_connected(from_node: nuke.Node, to_node: nuke.Node) -> bool:
+def is_connected(from_node: nuke.Node, to_node: nuke.Node, at: None | int = None) -> bool:
     """Check if a connection between the ``from_node`` to the ``to_node`` exists.
 
     Args:
         from_node: Node that should be connected to an input of ``to_node``.
         to_node: The node where the ``from_node`` is connected to.
+        at: Specific index to check.
 
     Returns:
         ``True`` if the two nodes are connected, else ``False``.
     """
-    return any(from_node is to_node.input(i) for i in range(to_node.inputs()))
+    if at is None:
+        return any(from_node is to_node.input(i) for i in range(to_node.inputs()))
+    if at < 0 or at > to_node.maximumInputs():
+        msg = f"The specified input index '{at}' is not available for this node: '{to_node.name()}'"
+        raise IndexError(msg)
+    return to_node.input(at) is from_node
