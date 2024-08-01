ab52e9126f1c91c52d08d326098e0a41932febae
Gilles Vink <gilles@vinkvfx.com>
2024-08-01 20:05:45 +0200

    Add cli interface

--- tests/src/nuketesting/_cli/test_cli.py
+++ tests/src/nuketesting/_cli/test_cli.py
@@ -2,30 +2,25 @@
 
 from __future__ import annotations
 
-import inspect
-import subprocess
-import sys
 from pathlib import Path
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 import pytest
-
-from datamodel import constants
-from _cli.runner import Runner
+from click.testing import CliRunner
+from nuketesting._cli.main import CLICommandError, CLIRunArguments, main
+from nuketesting._cli.runner import Runner
 
 pytest.importorskip(
-    "run_tests",
+    "nuketesting._cli",
     reason="This module is not importable by the nuke runtime. "
     "Skip tests of this module when executing them with nuke.",
 )
-
-from run_tests import run_tests
 
 
 @pytest.fixture()
 def runner() -> MagicMock:
     """Mock for the runner class."""
-    with patch("run_tests.Runner", spec=Runner) as runner_mock:
+    with patch("nuketesting._cli.main.Runner", spec=Runner) as runner_mock:
         yield runner_mock
 
 
@@ -36,89 +31,138 @@ def sys_exit() -> MagicMock:
         yield good_bye
 
 
-def test_create_runner(runner: MagicMock) -> None:
-    """Test that a runner instance is created from the user path."""
-    run_tests("nuke_path", ".")
+def test_pass_arguments_to_data_object() -> None:
+    """Test simple pass of arguments to dataclass."""
+    cli_testrunner = CliRunner()
 
-    runner.assert_called_with("nuke_path")
+    with patch("nuketesting._cli.main.CLIRunArguments") as test_run_arguments_mock:
+        cli_testrunner.invoke(main, ["-n", "nuke_path"])
+
+    test_run_arguments_mock.assert_called_once_with(
+        nuke_executable="nuke_path",
+        test_directory=".",
+        config=None,
+        run_in_terminal_mode=True,
+        pytest_args=(),
+        runner_name=None,
+    )
+
+
+def test_pass_all_arguments_to_data_object() -> None:
+    """Test passing of all possible arguments to dataclass object."""
+    cli_testrunner = CliRunner()
+
+    with patch("nuketesting._cli.main.CLIRunArguments") as test_run_arguments_mock:
+        cli_testrunner.invoke(
+            main,
+            [
+                "-n",
+                "nuke_path",
+                "-t",
+                "test_dir",
+                "-c",
+                "config/path.json",
+                "--terminal",
+                "false",
+                "-p",
+                "-v test",
+                "-p",
+                "-x",
+                "-r",
+                "Boomer",
+            ],
+        )
+
+    test_run_arguments_mock.assert_called_once_with(
+        nuke_executable="nuke_path",
+        test_directory="test_dir",
+        config="config/path.json",
+        run_in_terminal_mode=False,
+        pytest_args=("-v test", "-x"),
+        runner_name="Boomer",
+    )
+
+
+def test_run_arguments_convert_to_path() -> None:
+    """Test that the CLIRunArguments converts the paths to pathlib."""
+    test_cli_run_arguments = CLIRunArguments("test", (), "executable", "config")
+
+    assert test_cli_run_arguments.test_directory == Path("test")
+    assert test_cli_run_arguments.nuke_executable == Path("executable")
+    assert test_cli_run_arguments.config == Path("config")
+
+
+def test_exception_when_not_enough_arguments() -> None:
+    """Test to raise a TestRunCommandError when neither exe or config is provided."""
+    with pytest.raises(CLICommandError, match="Neither a config or a Nuke executable is provided."):
+        CLIRunArguments(".")
+
+
+def test_logger_called_during_error() -> None:
+    with patch("nuketesting._cli.main.logger") as logger_mock:
+        cli_testrunner = CliRunner()
+        cli_testrunner.invoke(main, [])
+
+    logger_mock.error.assert_called_once_with("An error occured: 'Neither a config or a Nuke executable is provided.'")
 
 
 def test_runner_executed(runner: MagicMock) -> None:
     """Test that the runner is executed."""
     instance = runner.return_value
-    run_tests("nuke_path", ".")
+    cli_testrunner = CliRunner()
 
-    instance.execute_tests.assert_called_once_with(".")
+    cli_testrunner.invoke(main, ["-n nuke_path"])
+
+    instance.execute_tests.assert_called_once_with(Path())
 
 
 def test_exit_code_forwarding(runner: MagicMock, sys_exit: MagicMock) -> None:
     """Test that the return code of the runner is forwarded to the caller."""
     instance = runner.return_value
     instance.execute_tests.return_value = 1928
+    cli_testrunner = CliRunner()
 
-    run_tests("nuke_path", ".")
+    cli_testrunner.invoke(main, ["-n", "nuke_path"])
 
-    sys_exit.assert_called_with(1928)
+    assert sys_exit.call_args_list[0] == call(
+        1928
+    )  # As the CLIRunner object from click is returning 0 as exit code always.
 
 
-@patch("run_tests.find_configuration", MagicMock(spec=str))
-@patch("run_tests.load_runners")
+@patch("nuketesting._cli.main.find_configuration", MagicMock(spec=str))
+@patch("nuketesting._cli.main.load_runners")
 def test_config_file_loaded(load_config: MagicMock, runner: MagicMock) -> None:
     """Test that runners from the config are prioritized."""
     my_runner = MagicMock(spec=Runner)
     load_config.return_value = {"my_runner": my_runner}
 
-    run_tests("my_runner", "test_path")
+    arguments = CLIRunArguments(".", nuke_executable="test.exe", runner_name="my_runner")
+    arguments.run_tests()
+    my_runner.execute_tests.assert_called_with(Path())
 
-    my_runner.execute_tests.assert_called_with("test_path")
+
+@patch("nuketesting._cli.main.find_configuration", MagicMock(spec=str))
+@patch("nuketesting._cli.main.load_runners")
+def test_config_file_preferred_with_specified_json(load_config: MagicMock, runner: MagicMock) -> None:
+    """Test that runners from the json are prioritized."""
+    my_runner = MagicMock(spec=Runner)
+    load_config.return_value = {"my_runner": my_runner}
+
+    arguments = CLIRunArguments(".", config="test.json", runner_name="my_runner")
+    arguments.run_tests()
+
+    load_config.assert_called_once_with(Path("test.json"))
 
 
-@patch("run_tests.find_configuration")
-@patch("run_tests.load_runners")
+@patch("nuketesting._cli.main.find_configuration")
+@patch("nuketesting._cli.main.load_runners")
 def test_search_for_config_used(load_config: MagicMock, find_config: MagicMock, runner: MagicMock) -> None:
     """Test that the test file is used to find the config."""
-    run_tests("my_runner", "path/to/test.py")
+    arguments = CLIRunArguments(
+        "path/to/test.py",
+        nuke_executable="something",
+    )
+    arguments.run_tests()
 
     find_config.assert_called_once_with(Path("path/to/test.py"))
     load_config.assert_called_once_with(find_config.return_value)
-
-
-@pytest.mark.nuke()
-@pytest.mark.slow()
-@pytest.mark.parametrize(
-    ("test", "code"),
-    [("test_failing", 1), ("test_passing", 0), ("not_existing", 4)],
-)
-def test_commandline(test: str, code: int) -> None:
-    """Test that the script can be executed from the commandline."""
-    run_file = inspect.getfile(run_tests)
-    call = [sys.executable, run_file]
-    tests_folder = constants.NUKE_TESTING_FOLDER / "tests"
-    reference_test = tests_folder / f"reference_tests.py::{test}"
-
-    call.extend(["nuke", str(reference_test)])
-
-    print(call)  # noqa: T201
-    process = subprocess.run(call, stdout=subprocess.PIPE, check=False)
-    # Used for debugging subprocess output:
-    print(process.stdout.decode())  # noqa: T201
-
-    assert process.returncode == code
-
-
-@pytest.mark.parametrize(
-    ("args", "message"),
-    [
-        ([], "Missing argument 'INTERPRETER'"),
-        (["."], "Missing argument 'TESTS'"),
-    ],
-)
-def test_commandline_missing_parameters(args: list[str], message: str) -> None:
-    """Test that the nuke path and the tests are required."""
-    run_file = inspect.getfile(run_tests)
-    call = [sys.executable, run_file]
-
-    call.extend(args)
-    process = subprocess.run(call, stderr=subprocess.PIPE, check=False)
-
-    assert message in process.stderr.decode()
