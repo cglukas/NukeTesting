3a12570a2dbc0adaf15264a4d35789154b06dfc4
Gilles Vink <gilles@vinkvfx.com>
2024-08-01 20:06:38 +0200

    Simplify constants and add tests script to bootstrap py

--- src/nuketesting/datamodel/constants.py
+++ src/nuketesting/datamodel/constants.py
@@ -2,7 +2,4 @@ from pathlib import Path
 
 NUKE_TESTING_FOLDER = Path(__file__).parent.parent
 
-NUKE_DEPENDENCY_FOLDER = NUKE_TESTING_FOLDER / "nuke_dependencies"
-REQUIREMENT_TXT = NUKE_TESTING_FOLDER / "requirements.txt"
-
-RUN_TESTS_SCRIPT = NUKE_TESTING_FOLDER / "nuke_test_runner" / "run_pytest.py"
+RUN_TESTS_SCRIPT = NUKE_TESTING_FOLDER / "_cli" / "run_pytest_bootstrapped.py"
